5ea949b3a3cef61566b09a269ecaa13c32defb51
an.zeitler <an.zeitler@stecx.de>
2026-07-16 16:08:27 +0200

    - fixed full type string generation

--- src/Rappd.Data.Generators/Utils/SourceGeneratorUtil.cs
+++ src/Rappd.Data.Generators/Utils/SourceGeneratorUtil.cs
@@ -20,9 +20,6 @@ namespace Rappd.Data.Generators.Utils
                 _ => "public"
             };
 
-        private static string GetNamespace(INamespaceSymbol @namespace)
-            => @namespace.IsGlobalNamespace ? "" : $"{@namespace}.";
-
         private static string GetValue(object? value)
             => value switch
             {
@@ -52,7 +49,7 @@ namespace Rappd.Data.Generators.Utils
                     keyword = "class";
                 }
 
-                sb.AppendLine($"    {GetAccessibility(type.Accessibility)} partial {keyword} {type.Name} : {string.Join(", ", typeToGenerate.InterfacesToImplement.Select(i => $"{GetNamespace(i.ContainingNamespace)}{i.Name}"))}");
+                sb.AppendLine($"    {GetAccessibility(type.Accessibility)} partial {keyword} {type.Name} : {string.Join(", ", typeToGenerate.InterfacesToImplement.Select(i => i.ToDisplayString()))}");
                 sb.AppendLine("    {");
                 foreach (var member in typeToGenerate.MembersToGenerate)
                 {
@@ -60,18 +57,18 @@ namespace Rappd.Data.Generators.Utils
                     {
                         case PropertyToGenerate propertyToGenerate:
                             var property = propertyToGenerate.Property;
-                            sb.AppendLine($"        {GetAccessibility(property.DeclaredAccessibility)}{(property.IsStatic ? " static" : "")} {GetNamespace(property.Type.ContainingNamespace)}{property.Type.Name} {property.Name} {{ get; {(property.SetMethod is null ? "init" : "set")}; }}{(propertyToGenerate.Value is not null ? $" = {GetValue(propertyToGenerate.Value)};" : "")}");
+                            sb.AppendLine($"        {GetAccessibility(property.DeclaredAccessibility)}{(property.IsStatic ? " static" : "")} {property.Type.ToDisplayString()} {property.Name} {{ get; {(property.SetMethod is null ? "init" : "set")}; }}{(propertyToGenerate.Value is not null ? $" = {GetValue(propertyToGenerate.Value)};" : "")}");
                             break;
 
                         case MethodToGenerate methodToGenerate:
                             var method = methodToGenerate.Method;
-                            string returnType = method.ReturnsVoid ? "void" : $"{GetNamespace(method.ReturnType.ContainingNamespace)}{method.ReturnType.Name}";
+                            string returnType = method.ReturnsVoid ? "void" : method.ReturnType.ToDisplayString();
                             sb.Append($"        {GetAccessibility(method.DeclaredAccessibility)}{(method.IsStatic ? " static" : "")} {returnType} {method.Name}");
                             if (method.TypeParameters.Any())
                             {
                                 sb.Append($"<{string.Join(", ", method.TypeParameters.Select(p => $"{p.Name}"))}>");
                             }
-                            sb.AppendLine($"({string.Join(", ", method.Parameters.Select(p => $"{GetNamespace(p.Type.ContainingNamespace)}{p.Type.Name} {p.Name}"))})");
+                            sb.AppendLine($"({string.Join(", ", method.Parameters.Select(p => p.Type.ToDisplayString()))})");
                             sb.AppendLine("            => throw new NotImplementedException();");
                             break;
                     }
@@ -98,7 +95,7 @@ namespace Rappd.Data.Generators.Utils
             sb.AppendLine($"        public static void Register{implementation.Name}()");
             sb.AppendLine("        {");
             foreach (var @interface in typeToGenerate.InterfacesToImplement)
-                sb.AppendLine($"            {typeof(KnownTypesRegistry).FullName}.{nameof(KnownTypesRegistry.Instance)}.{nameof(KnownTypesRegistry.Instance.RegisterImplementation)}(typeof({GetNamespace(@interface.ContainingNamespace)}{@interface.Name}),typeof({implementation.ContainingNamespace}.{implementation.Name}));");
+                sb.AppendLine($"            {typeof(KnownTypesRegistry).FullName}.{nameof(KnownTypesRegistry.Instance)}.{nameof(KnownTypesRegistry.Instance.RegisterImplementation)}(typeof({@interface.ToDisplayString()}),typeof({implementation.ContainingNamespace}.{implementation.Name}));");
             sb.AppendLine("        }");
             sb.AppendLine("    }");
             sb.AppendLine("}");
@@ -119,7 +116,7 @@ namespace Rappd.Data.Generators.Utils
             sb.AppendLine("        [System.Runtime.CompilerServices.ModuleInitializer]");
             sb.AppendLine($"        public static void RegisterBase{interfaceType.Name}()");
             sb.AppendLine("        {");
-            sb.AppendLine($"            {typeof(KnownTypesRegistry).FullName}.{nameof(KnownTypesRegistry.Instance)}.{nameof(KnownTypesRegistry.Instance.RegisterBaseType)}(typeof({GetNamespace(interfaceType.ContainingNamespace)}{interfaceType.Name}),(typeof({propertySymbol.Type.ContainingNamespace}.{propertySymbol.Type.Name}),nameof({GetNamespace(interfaceType.ContainingNamespace)}{interfaceType.Name}.{propertySymbol.Name})));");
+            sb.AppendLine($"            {typeof(KnownTypesRegistry).FullName}.{nameof(KnownTypesRegistry.Instance)}.{nameof(KnownTypesRegistry.Instance.RegisterBaseType)}(typeof({interfaceType.ToDisplayString()}),(typeof({propertySymbol.Type.ContainingNamespace}.{propertySymbol.Type.Name}),nameof({interfaceType.ToDisplayString()}.{propertySymbol.Name})));");
             sb.AppendLine("        }");
             sb.AppendLine("    }");
             sb.AppendLine("}");
@@ -141,7 +138,7 @@ namespace Rappd.Data.Generators.Utils
             sb.AppendLine("        [System.Runtime.CompilerServices.ModuleInitializer]");
             sb.AppendLine($"        public static void RegisterSub{subInterfaceSymbol.Name}()");
             sb.AppendLine("        {");
-            sb.AppendLine($"            {typeof(KnownTypesRegistry).FullName}.{nameof(KnownTypesRegistry.Instance)}.{nameof(KnownTypesRegistry.Instance.RegisterSubType)}(typeof({GetNamespace(baseInterfaceType.ContainingNamespace)}{baseInterfaceType.Name}), {GetValue(discriminatorValue)}, typeof({GetNamespace(subInterfaceSymbol.ContainingNamespace)}{subInterfaceSymbol.Name}));");
+            sb.AppendLine($"            {typeof(KnownTypesRegistry).FullName}.{nameof(KnownTypesRegistry.Instance)}.{nameof(KnownTypesRegistry.Instance.RegisterSubType)}(typeof({baseInterfaceType.ToDisplayString()}), {GetValue(discriminatorValue)}, typeof({subInterfaceSymbol.ToDisplayString()}));");
             sb.AppendLine("        }");
             sb.AppendLine("    }");
             sb.AppendLine("}");
@@ -161,8 +158,8 @@ namespace Rappd.Data.Generators.Utils
             sb.AppendLine("    {");
             foreach (var @interface in typeToGenerate.InterfacesToImplement)
             {
-                sb.AppendLine($"        public static {GetNamespace(@interface.ContainingNamespace)}{@interface.Name} Create<TInterface>({implementation.ContainingNamespace}.{implementation.Name} implementation)");
-                sb.AppendLine($"            where TInterface : {GetNamespace(@interface.ContainingNamespace)}{@interface.Name}");
+                sb.AppendLine($"        public static {@interface.ToDisplayString()} Create<TInterface>({implementation.ContainingNamespace}.{implementation.Name} implementation)");
+                sb.AppendLine($"            where TInterface : {@interface.ToDisplayString()}");
                 sb.AppendLine("        {");
                 sb.AppendLine($"            return implementation;");
                 sb.AppendLine("        }");
