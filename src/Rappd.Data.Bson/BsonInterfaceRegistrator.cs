d857f3aac95cc69880fd52c7049022978c261b39
an.zeitler <an.zeitler@stecx.de>
2026-07-26 12:59:47 +0200

    - cc

---
+++ src/Rappd.Data.Bson/BsonInterfaceRegistrator.cs
@@ -0,0 +1,31 @@
+﻿using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rappd.Data.Bson;
+
+public static class BsonInterfaceRegistrator
+{
+    public static void RegisterInterfaces()
+    {
+        foreach (var interfaceImplementation in KnownTypesRegistry.Instance.GetInterfaceImplementations())
+        {
+            var serializer = typeof(ImpliedImplementationInterfaceSerializer<,>).MakeGenericType(interfaceImplementation.@interface, interfaceImplementation.implementation);
+            BsonSerializer.RegisterSerializer(interfaceImplementation.@interface, Activator.CreateInstance(serializer) as IBsonSerializer);
+        }
+
+        foreach (var polymorphicType in KnownTypesRegistry.Instance.GetPolymorphicTypes())
+        {
+            var baseMap = BsonClassMap.LookupClassMap(polymorphicType.@base);
+            baseMap.SetDiscriminatorIsRequired(true);
+            foreach (var subType in polymorphicType.subtypes)
+            {
+                baseMap.AddKnownType(subType.subtype);
+                var subMap = BsonClassMap.LookupClassMap(subType.subtype);
+                subMap.SetDiscriminator(subType.discriminator.ToString());
+            }
+        }
+    }
+}
